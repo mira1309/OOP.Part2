0139bd44d9923f6e38bd29ffc596bc33f59129f9
Ильмира <kushkarova.1998@mail.ru>
2023-10-13 18:06:26 +0300

    finish code

---
+++ src/Bicycle.java
@@ -0,0 +1,17 @@
+public class Bicycle extends Transport {
+
+    public Bicycle(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+
+    public void doService() {
+    System.out.println ("Обслуживаем " + this.getModelName());
+    updateTyre();
+    }
+
+    public void updateTyre() {
+        for (int i = 0; i < this.getWheelsCount(); i++) {
+            System.out.println("Меняем покрышку");
+        }
+    }
+}
